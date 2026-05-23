88500cb347a648eff5db8931309b1c0f0552a126
suwapotta <nguyenducthientan09@gmail.com>
2026-05-23 08:04:52 +0700

    Fix extra line for icarus run test
    Signed-off-by: suwapotta <nguyenducthientan09@gmail.com>

--- src/IR.v
+++ src/IR.v
@@ -8,10 +8,15 @@ module instruction_register (
 );
 
   // 1. TODO: Declare an internal register to store the instruction data
+
   // 2. TODO: Implement a sequential logic block triggered by the positive edge of the clock
+
   // 3. TODO: Implement synchronous active-high reset to clear the register
+
   // 4. TODO: Implement logic to load data_in into the register when ld_ir is active
+
   // 5. TODO: Implement combinational logic to extract the 3-bit opcode from the stored instruction
+
   // 6. TODO: Implement combinational logic to extract the 5-bit operand from the stored instruction
 
 endmodule
