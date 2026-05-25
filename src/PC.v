f1aa355257415538113f31359417ae248f8fb1d5
VinhTechiee <lehienvinh06@gmail.com>
2026-05-25 20:10:34 +0700

    Update introduction and implement PC and address mux

--- src/PC.v
+++ src/PC.v
@@ -10,4 +10,20 @@ module PC(
     // 2. TODO: Implement synchronous active-high reset to clear the program counter
     // 3. TODO: Implement logic to load a new address into the program counter when ld_pc is active
     // 4. TODO: Implement logic to increment the program counter when inc_pc is active
+
+    always @(posedge clk) begin
+        if (rst) begin
+            pc_out <= 32'd0;
+        end
+        else if (ld_pc) begin
+            pc_out <= data_in;
+        end
+        else if (inc_pc) begin
+            pc_out <= pc_out + 32'd1;
+        end
+        else begin
+            pc_out <= pc_out;
+        end
+    end
+    
 endmodule
