7562b648f321328cc0b8fa7ab6ef05e55ea49538
ladonna-2511 <dangngoclananh251107@gmail.com>
2026-05-29 11:34:54 +0700

    Update ALU.v

--- src/ALU.v
+++ src/ALU.v
@@ -1,13 +1,31 @@
-module alu (
-    input [31:0] inA,
-    input [31:0] inB, 
-    input [2:0] opcode,  
-    output reg [31:0] alu_out,
-    output zero 
-);
+
     // 1. TODO: Implement combinational logic to set the zero flag if inA is 0
     // 2. TODO: Implement combinational logic to perform arithmetic and logic operations based on the opcode
     // 3. TODO: Define operations for HLT, SKZ, ADD, AND, XOR, LDA, STO, JMP instructions
     // 4. TODO: Assign the result to alu_out
+module ALU(
+input [2:0] opcode,
+input [7:0] inA,
+input [7:0] inB,
 
+output reg [7:0] alu_out,
+output zero
+   );
+   always @(*) begin
+   case(opcode)
+   3'b000: alu_out = inA;
+   3'b001: alu_out = inA;
+   3'b010: alu_out = inA + inB;
+   3'b011: alu_out = inA & inB;
+   3'b100: alu_out = inA ^ inB;
+   3'b101: alu_out = inB;
+   3'b110: alu_out = inA;
+   3'b111: alu_out = inA;
+   default: alu_out = 8'b00000000;
+   endcase
+   end
+   assign zero = (inA == 8'b00000000); 
+   
 endmodule
+
+
